849405ee0177b99967dcd48232cf3a233c748b4d
codeadamca <thomasadam83@hotmail.com>
2022-12-08 02:23:19 +0000

    Added touch sample code

---
+++ main.py
@@ -0,0 +1,41 @@
+import os, time, brickpi3
+
+# Initialize the EV3 Brick
+BP = brickpi3.BrickPi3()
+
+# Initialize EV3 touch sensor and motors
+BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.TOUCH)
+touchButton = 0
+
+# Without a delay reading sensor data can cause an error
+time.sleep(0.5)
+
+# Quite loop when CTRL+C is pressed
+try:
+
+    # Create a loop to react to buttons
+    while True:
+
+        # Get touch sensor status
+        touchButton = BP.get_sensor(BP.PORT_1)
+            
+        # If the touch sensor is pressed
+        if touchButton:
+
+            # Turn on Raspberry Pi on board LED
+            os.system('echo 1 | sudo dd status=none of=/sys/class/leds/led0/brightness') 
+        
+        # If the touch sensor is released
+        else:
+
+            # Turn off Raspberry Pi on board LED
+            os.system('echo 0 | sudo dd status=none of=/sys/class/leds/led0/brightness') 
+
+        # Loop delay
+        time.sleep(0.1)
+
+# Result of CTRL+C
+except KeyboardInterrupt:
+    
+    # Unconfigure the sensors
+    BP.reset_all() 
